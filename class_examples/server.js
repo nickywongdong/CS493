e4393fde2b169f26cee81ba93255783a9abf20d4
wongnich <wongnich@gmail.com>
2018-04-11 14:54:04 -0700

    updating gitignore

--- class_examples/server.js
+++ class_examples/server.js
@@ -2,8 +2,19 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/lodgings', function (req, res, next) {
-  res.status(200).send("Status 200!\n");
+app.get('/', function (req, res, next) {
+  res.status(200).send("Status 200\n");
+});
+
+app.get('/lodgings/:lodgingID', function (req, res, next) {
+  console.log("  -- req.params:", req.params);
+  res.status(200).end();
+});
+
+app.get('*', function (req, res, next) {
+  res.status(404).json({
+    err: "Path " + req.url + " does not exist"
+  });
 });
 
 app.listen(port, function() {
